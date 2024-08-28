9e1bce541e52ef5a4ed5f0e272906c3d03654139
E.Arjun Reddy <119129042+arjunreddy25@users.noreply.github.com>
2024-08-29 01:04:14 +0530

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES="0,1" \
 python train.py \
---hiera_path "<set your pretrained hiera path here>" \
---train_image_path "<set your training image dir here>" \
+--hiera_path "/kaggle/input/segment/sam2_hiera_large.pt" \
+--train_image_path "" \
 --train_mask_path "<set your training mask dir here>" \
 --save_path "<set your checkpoint saving dir here>" \
 --epoch 20 \
